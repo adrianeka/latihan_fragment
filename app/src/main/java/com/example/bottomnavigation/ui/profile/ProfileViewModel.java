2fd90e159b43a7cf7a53b5d50de06a06e4b64519
AdrianEka <adrian.eka9@gmail.com>
2021-11-04 13:35:53 +0700

    latihan_fragment_AdrianEka

---
+++ app/src/main/java/com/example/bottomnavigation/ui/profile/ProfileViewModel.java
@@ -0,0 +1,18 @@
+package com.example.bottomnavigation.ui.profile;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public ProfileViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("profile");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
